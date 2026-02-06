2039539926076f98032407bc0ff726f40a599512
mandy <67531582+MandyIGuess@users.noreply.github.com>
2026-02-06 17:36:46 -0500

    Remove an OSReport i left in

--- Kamek/src/spritetex.cpp
+++ Kamek/src/spritetex.cpp
@@ -4,5 +4,4 @@ void sandPillarZOrder(dStageActor_c *self, int param_2) {
     self->checkZoneBoundaries(param_2); // replaced
 
     self->pos.z = -6000.0f;
-    OSReport("sandpillar Z pos: %f\n", self->pos.z);
 }
